b92baebcac8c19a9c39b9d516b71f28e7b91e1e5
Walheimat <krister.schuchardt@gmail.com>
2023-06-24 17:15:51 +0200

    chore: update dinghy

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,2 @@
-((nil . ((wal-project-test-default-cmd . "make local-test")))
- (emacs-lisp-mode . ((find-sibling-rules . (("test/bydi-test.el" "bydi.el")
+((emacs-lisp-mode . ((find-sibling-rules . (("test/bydi-test.el" "bydi.el")
                                             ("bydi.el" "test/bydi-test.el"))))))
