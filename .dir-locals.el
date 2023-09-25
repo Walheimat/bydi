fae2f36561a429fe72470fb03f74b51b44fab0cb
Walheimat <krister.schuchardt@gmail.com>
2023-09-25 19:09:33 +0200

    chore: update ignores

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-((nil . ((wal-project-execute-default-cmd . ("make update-version"
-                                             "make pacify"
-                                             "git submodulde update --remote"))
-         (find-sibling-rules . (("test/bydi-test.el" "bydi.el")
-                                ("bydi.el" "test/bydi-test.el"))))))
