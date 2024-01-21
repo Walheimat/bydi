faea0689c6a8e87159127b0584f5c1ca550773fe
Walheimat <krister.schuchardt@gmail.com>
2024-01-21 19:44:35 +0100

    release: v0.6.4

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PACKAGE_NAME=bydi
 
-CURRENT_PACKAGE_VERSION=0.6.3
+CURRENT_PACKAGE_VERSION=0.6.4
 
 include dinghy/emacs-package.mk
--- bydi.el
+++ bydi.el
@@ -2,7 +2,7 @@
 
 ;; Author: Krister Schuchardt <krister.schuchardt@gmail.com>
 ;; Homepage: https://github.com/Walheimat/bydi
-;; Version: 0.6.3
+;; Version: 0.6.4
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: extensions
 
